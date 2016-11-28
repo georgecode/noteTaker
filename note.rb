f391fdf1fc4550f19b17543ef1ed1ba4e44d57ed
georgecode <georgeczurlanis@gmail.com>
2016-11-28 03:01:44 -0800

    Add category_options method

--- note.rb
+++ note.rb
@@ -40,8 +40,39 @@ def new_note(db)
   create_note(db, category, name, note)
 end
 
-def search_note
+#SELECT name, age FROM table_name;
+#db.execute(SELECT name, age FROM table_name;)
 
+# explore ORM by retrieving data
+# kittens = db.execute("SELECT * FROM kittens")
+# kittens.each do |kitten|
+#  puts "#{kitten['name']} is #{kitten['age']}"
+# end
+def category_options(db)
+  puts "\nChoose a catagory"
+  my_category = db.execute("SELECT * FROM notes")
+  options = []
+  my_category.each do |category|
+    options << category[1]
+  end
+  puts options.uniq!
+end
+#array.uniq!
+
+# a = [ "a", "b", "c" ]
+# a.include?("b")   #=> true
+# a.include?("z")   #=> false
+def catagory_results(db)
+
+end
+
+
+
+
+
+
+def search_note(db)
+  puts "What category"
 end
 
 def initial_prompt(db)
@@ -52,13 +83,13 @@ def initial_prompt(db)
   if new_search.downcase == "new"
     new_note(db)
   elsif new_search.downcase == "search"
-    puts "good search"
+    category_options(db)
   elsif new_search.downcase == "exit"
     puts "See you soon!"
   else
     spacer
     puts "\nThats not a choice\n"
-    initial_prompt
+    initial_prompt(db)
   end
 end
 
